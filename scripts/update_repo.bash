93aa92e9adc96ef351e1bec33ebcd170c2689d07
William Ma <12377941+williamma12@users.noreply.github.com>
2020-10-29 15:40:56 -0500

    Update dotfiles

--- scripts/update_repo.bash
+++ scripts/update_repo.bash
@@ -2,8 +2,8 @@
 
 cp ~/.vimrc files/vimrc
 cp ~/.tmux.conf files/tmux.conf
-cp ~/.config/nvim/ files/config/nvim
-cp ~/.config/bash/ files/config/bash
+cp -r ~/.config/nvim/ files/config/nvim
+cp -r ~/.config/bash/ files/config/bash
 
 git commit -a -m 'Update dotfiles'
 git push origin master
